3e9465f6581765df145826c0dfd173872c9a7d84
miloszKubiak <milypan69@gmail.com>
2021-12-30 12:52:25 +0100

    add filter function and update Home template

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,25 @@
 <template>
-  <div>
-    <h1>hello</h1>
-    {{ pokemons }}
+  <div class="flex justify-center bg-blue-500">
+    <div class="w-2/4 flex flex-col justify-center items-center border-4 border-yellow-500">
+      <div class="bg-red-500 w-full flex justify-center">
+        <input type="text" placeholder="Enter Pokemon here..." class="outline-none mt-10 p-2 border-2 border-blue-500"
+          v-model="text">
+      </div>
+      <div class="mt-10 p-4 flex flex-wrap justify-center bg-green-500">
+        <div class="ml-4 text-2xl text-blue-200"
+          v-for="(pokemon, index) in filteredPokemon"
+          :key="index"
+          >
+          {{ pokemon.name }}
+
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import { reactive, toRefs } from '@vue/reactivity'
+import { computed, reactive, toRefs } from '@vue/reactivity'
 
 
 export default {
@@ -15,8 +28,18 @@ export default {
 
     const state = reactive({
       pokemons: [],
-      urlId: {}
+      urlId: {},
+      text: "",
+      filteredPokemon:computed(() => updatePokemon())
     })
+
+    function updatePokemon() {
+      if(!state.text) {
+        return[]
+      }
+
+      return state.pokemons.filter(pokemon => pokemon.name.includes(state.text))
+    }
     
     fetch("https://pokeapi.co/api/v2/pokemon?offset=0")
       .then(res => res.json())
