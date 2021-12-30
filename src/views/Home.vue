600fd774ee21425378b68c7fa78e0257623965b2
miloszKubiak <milypan69@gmail.com>
2021-12-30 12:11:58 +0100

    add loading data in Home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,34 @@
 <template>
   <div>
     <h1>hello</h1>
+    {{ pokemons }}
   </div>
 </template>
 
 <script>
+import { reactive, toRefs } from '@vue/reactivity'
 
 
 export default {
   name: 'Home',
-  components: {
+  setup() {
+
+    const state = reactive({
+      pokemons: [],
+      urlId: {}
+    })
     
+    fetch("https://pokeapi.co/api/v2/pokemon?offset=0")
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+        state.pokemons = data.results;
+        state.urlId = data.results.reduce((accumulator, current, index) =>
+        accumulator = {...accumulator, [current.name]:index + 1}
+        ,{})
+      })
+
+      return {...toRefs(state)}
   }
 }
 </script>
